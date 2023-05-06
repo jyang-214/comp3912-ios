e6d7bd8bf2d595eef0a5d4c3b87b89355119f489
jyang-214 <jyang214@my.bcit.ca>
2023-05-06 10:20:56 -0700

    MyMemoryGame created

---
+++ MyMemoryGame/MyMemoryGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMemoryGame
+//
+//  Created by Jacky Yang on 2023-05-06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
