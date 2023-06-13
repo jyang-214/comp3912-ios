ecbde18f63bf198601dda718b5d9ed1c66d5ba17
jyang-214 <jyang214@my.bcit.ca>
2023-06-12 19:29:06 -0700

    iOS Assignment 2

---
+++ iOS-assignment2/iOS-assignment1/iOS-assignment1/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  iOS-assignment1
+//
+//  Created by Jacky Yang on 2023-06-12.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
---
+++ iOS-assignment2/iOS-assignment1/iOS-assignment1/WelcomeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  WelcomeViewController.swift
+//  iOS-assignment1
+//
+//  Created by Jacky Yang on 2023-06-12.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func Login(_ sender: Any) {
+        print("Login has been clicked.")
+//        let storyboard = UIStoryboard(name: "Main", bundle: nil) // Replace "Main" with your storyboard name
+//            let navController = storyboard.instantiateViewController(withIdentifier: "LoginNavigationControllerID") as! UINavigationController
+//
+//        self.modalPresentationStyle = .fullScreen
+//            self.present(navController, animated: true, completion: nil)
+    }
+    
+}
